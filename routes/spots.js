223ce861c22e8e41725625f0a42015e320eb5095
Grant Groseclose <grantgroseclose@gmail.com>
2023-08-25 13:13:54 -0400

    fix coordinate validation

--- routes/spots.js
+++ routes/spots.js
@@ -19,8 +19,8 @@ const SpotModel = require('../models/Spots');
 const validationSchema = Joi.object({
     name: Joi.string().min(1).required(),
     description: Joi.string().min(1).required(),
-    latitude: Joi.string().min(5).required(),
-    longitude: Joi.string().min(5).required()
+    latitude: Joi.string().min(1).required(),
+    longitude: Joi.string().min(1).required()
 });
 
 const generateRandomFileKey = (currentName) => {
